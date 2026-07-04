f76f40e0e24cd1828052edf3c0381c78783b86fb
Filip Ilic <filip.ilic161@gmail.com>
2026-07-04 18:07:20 +0200

    ispravljena sintaksna greska u modelu company, dodata tacka-zarez

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'company_name'
+    ];
+
+    public function jobListings(){
+        return $this->hasMany(JobListings::class);
+    }
 }
